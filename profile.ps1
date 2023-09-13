84489e29e0dc08bdd557dd457fac7decc63dd86f
Robin Ridderholt <robinr@activesolution.se>
2023-09-13 15:47:01 +0200

    Updates

--- profile.ps1
+++ profile.ps1
@@ -1,14 +1,11 @@
-oh-my-posh --init --shell pwsh --config ~/craver.omp.json | Invoke-Expression
 Import-Module posh-git
+
+oh-my-posh init pwsh --config "C:/Users/robinr/easy.omp.json" | Invoke-Expression
+
 Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
-function RunKsqlMigrationsDocker() {
-	docker run -v $PWD/src:/share/ksql-migrations confluentinc/ksqldb-server:0.23.1 ksql-migrations $args
-}
-
-Set-Alias ksql-migrations RunKsqlMigrationsDocker
 Set-Alias -Value kubectl -Name k
